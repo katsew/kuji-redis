9b250a7dd4fa294ad9edd1f29fffc94e93610539
katsew <y.katsew@gmail.com>
2017-05-30 12:43:29 +0900

    add new strategy

---
+++ simple.go
@@ -0,0 +1,46 @@
+package kuji_redis
+
+import (
+	"github.com/go-redis/redis"
+	"github.com/katsew/kuji"
+)
+
+type SimpleStrategy struct {
+	client *redis.Client
+	util *kuji.Util
+}
+
+func NewSimpleStrategy(o *redis.Options) (SimpleStrategy) {
+	return SimpleStrategy{
+		client: redis.NewClient(o),
+		util: &kuji.Util{},
+	}
+}
+
+func (s SimpleStrategy) PickOneByKey(key string) (string, error) {
+	length := s.client.LLen(key)
+	pickingIndex := s.util.RandomNumberFromRange(0, length.Val())
+	res := s.client.LIndex(key, pickingIndex)
+	return res.Result()
+}
+
+func (s SimpleStrategy) PickOneByKeyAndIndex(key string, index int64) (string, error) {
+	res := s.client.LIndex(key, index)
+	return res.Result()
+}
+
+func (s SimpleStrategy) PickAndDeleteOneByKey(key string) (string, error) {
+	length := s.client.LLen(key)
+	pickingIndex := s.util.RandomNumberFromRange(0, length.Val())
+	res := s.client.LIndex(key, pickingIndex)
+	s.client.LRem(key, 1, pickingIndex)
+	return res.Result()
+}
+
+func (s SimpleStrategy) RegisterCandidatesWithKey(key string, candidates []kuji.KujiCandidate) (int64, error) {
+	var res *redis.IntCmd
+	for _, v := range s.util.SpreadCandidates(candidates) {
+		res = s.client.LPush(key, v)
+	}
+	return res.Result()
+}
---
+++ suffle.go
@@ -0,0 +1,43 @@
+package kuji_redis
+
+import (
+	"github.com/go-redis/redis"
+	"github.com/katsew/kuji"
+)
+
+type ShuffleStrategy struct {
+	client *redis.Client
+	util *kuji.Util
+}
+
+func NewShuffleStrategy(o *redis.Options) (ShuffleStrategy) {
+	return ShuffleStrategy{
+		client: redis.NewClient(o),
+		util: &kuji.Util{},
+	}
+}
+
+func (s ShuffleStrategy) PickOneByKey(key string) (string, error) {
+	length := s.client.LLen(key)
+	pickingIndex := s.util.RandomNumberFromRange(0, length.Val())
+	res := s.client.LIndex(key, pickingIndex)
+	return res.Result()
+}
+
+func (s ShuffleStrategy) PickOneByKeyAndIndex(key string, index int64) (string, error) {
+	res := s.client.LIndex(key, index)
+	return res.Result()
+}
+
+func (s ShuffleStrategy) PickAndDeleteOneByKey(key string) (string, error) {
+	res := s.client.LPop(key)
+	return res.Result()
+}
+
+func (s ShuffleStrategy) RegisterCandidatesWithKey(key string, c []kuji.KujiCandidate) (int64, error) {
+	var res *redis.IntCmd
+	for _, v := range s.util.Shuffle(s.util.SpreadCandidates(c)) {
+		res = s.client.RPush(key, v)
+	}
+	return res.Result()
+}
